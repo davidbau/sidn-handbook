a535666e4ba4144f4e1143307313014c2730644e
David Bau <david.bau@gmail.com>
2024-09-18 07:09:55 -0400

    Update.

--- src/colab_link.py
+++ src/colab_link.py
@@ -1,4 +1,10 @@
 from urllib.parse import urljoin
 
-def img(relative_path, width=50):
-    return f'<img src="{relative_path}" style="max-width:{width}%; width:{width * 10}px;" class="mx-auto d-block">'
+def colab_link(relative_path, link_text=None):
+    global pathname
+    base_colab_url = 'https://colab.research.google.com/github/davidbau/sidn-handbook/blob/main/public'
+    project_url = urljoin(pathname, relative_path)
+    full_url = base_colab_url + project_url
+    if link_text is None:
+        return full_url
+    return f'<a href="{full_url}">link_text</a>'
